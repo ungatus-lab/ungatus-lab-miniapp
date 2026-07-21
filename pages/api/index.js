b21f395d700faa14208671e88f475ac7b1127b8a
ungatus-lab <ungatus@gmail.com>
2026-07-21 15:56:11 +0300

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   res.setHeader("X-Frame-Options", "ALLOWALL");
   res.setHeader("Content-Security-Policy", "frame-ancestors *");
-  res.status(200).send("ok");
+  res.status(200).send("ok123");
 }
