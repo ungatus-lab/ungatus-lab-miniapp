81b6bf7a466a2bc4bfe619ed4a8cc992fcae750c
ungatus-lab <ungatus@gmail.com>
2026-07-21 14:33:06 +0300

    Create index.js

---
+++ pages/api/index.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+  res.setHeader("X-Frame-Options", "ALLOWALL");
+  res.setHeader("Content-Security-Policy", "frame-ancestors *");
+  res.status(200).send("ok");
+}
